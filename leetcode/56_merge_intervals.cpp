f70f79f0097a9e4392ca176a55aee208688ae7c5
adam <admr0805@gmail.com>
2026-09-06 04:50:56 -0400

    solved merge intervals

--- leetcode/56_merge_intervals.cpp
+++ leetcode/56_merge_intervals.cpp
@@ -1,11 +1,27 @@
 #include <vector>
+#include <algorithm> 
 
 using namespace std;
 
 class Solution {
 public:
 	vector<vector<int>> merge(vector<vector<int>>& intervals) {
-		// TODO: implement
-		return {};
+		sort(intervals.begin(), intervals.end());
+		vector<vector<int>> merged{intervals[0]};
+		
+		for(int i = 1; i < intervals.size();i++) {
+			auto& x = intervals[i]; 
+			auto& y = stack.back(); 
+
+			if(x[1] <= y[1]){
+				continue;
+			} else if(x[0] <= y[1]){
+				y[1] = x[1]; 
+			} else {
+				stack.push_back(x);
+			}
+		}
+
+		return stack; 
 	}
 };
