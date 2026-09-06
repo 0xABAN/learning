d6158d10b00ea08f9e3a3e273a078ffa0863c93b
adam <admr0805@gmail.com>
2026-09-06 04:51:43 -0400

    feat(leetcode): add insert interval scaffold
    add the standard solution signature for interval insertion practice

---
+++ leetcode/57_insert_interval.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+		// TODO: implement
+		return {};
+	}
+};
