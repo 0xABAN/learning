f3dd73bdb8425f23fd8041962a23b4c33cd44d1d
adam <admr0805@gmail.com>
2026-09-07 18:46:03 -0400

    feat(leetcode): scaffold remove nth node
    add the list node type and method signature for problem 19
    without including a solution

---
+++ leetcode/19_remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,14 @@
+struct ListNode {
+	int val;
+	ListNode* next;
+
+	ListNode(int val = 0, ListNode* next = nullptr) : val(val), next(next) {}
+};
+
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		// TODO: implement
+		return nullptr;
+	}
+};
