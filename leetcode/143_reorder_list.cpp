3c9e41b51be714702781acfdc5d55b5352cedc19
adam <admr0805@gmail.com>
2026-09-08 20:56:58 -0400

    feat(leetcode): scaffold reorder list
    add the list node type and method signature for problem 143
    without including a solution

---
+++ leetcode/143_reorder_list.cpp
@@ -0,0 +1,13 @@
+struct ListNode {
+	int val;
+	ListNode* next;
+
+	ListNode(int val = 0, ListNode* next = nullptr) : val(val), next(next) {}
+};
+
+class Solution {
+public:
+	void reorderList(ListNode* head) {
+		// TODO: implement
+	}
+};
