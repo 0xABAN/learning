849744843f02a53065bf7e4de49668e45e0c8055
adam <admr0805@gmail.com>
2026-09-10 15:57:21 -0400

    feat(leetcode): scaffold level order traversal
    add the tree node type and method signature for problem 102
    without including a solution

---
+++ leetcode/102_binary_tree_level_order_traversal.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode(int val = 0, TreeNode* left = nullptr, TreeNode* right = nullptr)
+		: val(val), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		// TODO: implement
+		return {};
+	}
+};
