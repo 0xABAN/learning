9965952660ac8e5af4329edbf4f868dd2173235c
adam <admr0805@gmail.com>
2026-09-09 20:47:51 -0400

    feat(leetcode): scaffold lru cache
    add the constructor and cache method signatures for problem 146
    without including a solution

---
+++ leetcode/146_lru_cache.cpp
@@ -0,0 +1,15 @@
+class LRUCache {
+public:
+	LRUCache(int capacity) {
+		// TODO: implement
+	}
+
+	int get(int key) {
+		// TODO: implement
+		return -1;
+	}
+
+	void put(int key, int value) {
+		// TODO: implement
+	}
+};
