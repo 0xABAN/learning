af1500f7a110fb220485bce0af32677939bd48e6
adam <admr0805@gmail.com>
2026-09-11 19:44:15 -0400

    feat(leetcode): solve valid parentheses
    validate bracket nesting with a stack and closing-to-opening map

--- leetcode/20_valid_parentheses.cpp
+++ leetcode/20_valid_parentheses.cpp
@@ -1,11 +1,48 @@
 #include <string>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
 	bool isValid(string s) {
-		// TODO: implement
-		return false;
+		// "()[]{}"
+		// open brackets are closed by same bracket type 
+		/*
+		* i can loop twice, outer for loop loops through individual brackets, 
+		* inner for loop scans for next open / close bracket type 
+		* we store the number still left open after this amount 
+		*
+		*  would need a map storing bracket types "{": "}"
+		*  
+		*  this would be O(N^2)
+		*
+		*  could possibly be optimized to O(N) 
+		*  likely using some sort of stack structure 
+		*
+		*  [{, }, [, ]]
+		*
+		*  we push the first seen bracket to some stack 
+		*
+		*  [{]
+		*
+		*  we're looking for the next char to be }, if it is, pop stack, if isn't, add new to stack
+		*
+		* "()[]{}" -> [(, ]
+		* */ 
+		
+		unordered_map<char, char> m = {{')', '('}, {'}', '{'}, {']', '['}};
+		vector<char> stack; 
+
+		for(char c : s){
+			if(stack.empty() || stack.back() != m[c])	{
+				stack.push_back(c); 
+				continue; 
+			}
+			stack.pop_back(); 
+		}
+		
+		return stack.empty();
 	}
 };
