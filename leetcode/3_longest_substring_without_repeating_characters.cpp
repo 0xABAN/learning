62ea7b41ab77652182242d8d8aee4bf70b15cba8
adam <admr0805@gmail.com>
2026-09-11 19:44:20 -0400

    feat(leetcode): solve longest substring
    maintain a sliding window of distinct characters to find the
    longest substring without repeated characters

--- leetcode/3_longest_substring_without_repeating_characters.cpp
+++ leetcode/3_longest_substring_without_repeating_characters.cpp
@@ -1,11 +1,27 @@
 #include <string>
+#include <unordered_set>
+#include <algorithm> 
 
 using namespace std;
 
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
-		// TODO: implement
-		return 0;
+		// wwkew 
+		// {w, k, e, 	
+		unordered_set<char> seen; 
+		int l = 0, ans = 0; 	
+		
+		for(int r = 0; r < s.size(); r++){
+			while(seen.contains(s[r])) {
+				seen.erase(s[l]);
+				l++; 
+			}
+
+			seen.insert(s[r]); // b
+			ans = max(ans,r-l+1); // 1 
+		}
+		
+		return ans;
 	}
 };
