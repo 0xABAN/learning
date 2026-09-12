c307b7a097c5349604073163355f0a2bfb4af1dd
adam <admr0805@gmail.com>
2026-09-12 19:03:19 -0400

    feat(leetcode): solve nodes at distance k
    build bidirectional tree connections and use breadth-first
    search to collect nodes at the requested target distance

--- leetcode/863_all_nodes_distance_k_in_binary_tree.cpp
+++ leetcode/863_all_nodes_distance_k_in_binary_tree.cpp
@@ -1,4 +1,8 @@
 #include <vector>
+#include <unordered_map>
+#include <unordered_set> 
+#include <deque> 
+#include <utility> 
 
 using namespace std;
 
@@ -13,7 +17,43 @@ struct TreeNode {
 class Solution {
 public:
 	vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-		// TODO: implement
-		return {};
+		unordered_map<TreeNode*, vector<TreeNode*>> graph; 
+		const auto& construct_graph = [&graph](auto&& self, TreeNode* node) -> void {
+			if(!node) return;
+			
+			if(node->left){
+				graph[node].push_back(node->left);
+				graph[node->left].push_back(node);
+			}
+			if(node->right){
+				graph[node].push_back(node->right);
+				graph[node->right].push_back(node);
+			}
+			
+			self(self, node->left);
+			self(self, node->right); 
+		}; 
+
+		construct_graph(construct_graph, root); 
+		
+		deque<pair<TreeNode*, int>> q{{target, 0}};
+		unordered_set<TreeNode*> seen; 
+		vector<int> ans; 
+	
+		while(!q.empty()){
+			auto [node, dist] = q.front(); 
+			q.pop_front(); 
+			
+			seen.insert(node); 
+			if(dist == k) ans.push_back(node->val); 
+			
+			for(TreeNode* neighbor : graph[node]){
+				if(!seen.contains(neighbor)) {
+					q.push_back({neighbor, dist+1});	
+				}
+			}
+		}
+		
+		return ans; 
 	}
 };
