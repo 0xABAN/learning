325ee811fe3c3bb05fe2681e7e17b70fa573908c
adam <admr0805@gmail.com>
2026-09-12 06:43:19 -0400

    feat(leetcode): scaffold maximum tree depth
    add the tree node type and method signature for problem 104
    without including a solution

---
+++ leetcode/104_maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,16 @@
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode(int val = 0, TreeNode* left = nullptr, TreeNode* right = nullptr)
+		: val(val), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+		// TODO: implement
+		return 0;
+	}
+};
