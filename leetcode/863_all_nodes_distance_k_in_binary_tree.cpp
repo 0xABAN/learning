1d0d12c5a22727aecb27d63294c09128d61cb570
adam <admr0805@gmail.com>
2026-09-12 18:16:50 -0400

    feat(leetcode): scaffold nodes at distance k
    add the tree node type and method signature for problem 863
    after checking solved and attempted leetcode history

---
+++ leetcode/863_all_nodes_distance_k_in_binary_tree.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+	vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+		// TODO: implement
+		return {};
+	}
+};
