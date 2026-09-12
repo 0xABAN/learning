647d5f5350c4c8545da52f6a865eb0dfe34bd33d
adam <admr0805@gmail.com>
2026-09-12 06:04:25 -0400

    feat(leetcode): solve lowest common ancestor
    find the lowest shared ancestor with recursive subtree results
    while supporting either target as the ancestor of the other

--- leetcode/236_lowest_common_ancestor_of_a_binary_tree.cpp
+++ leetcode/236_lowest_common_ancestor_of_a_binary_tree.cpp
@@ -1,16 +1,26 @@
-struct TreeNode {
-	int val;
+struct TreeNode{
+	int val; 
 	TreeNode* left;
-	TreeNode* right;
-
-	TreeNode(int val = 0, TreeNode* left = nullptr, TreeNode* right = nullptr)
-		: val(val), left(left), right(right) {}
+	TreeNode* right; 
+	
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
+
 
 class Solution {
 public:
 	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-		// TODO: implement
-		return nullptr;
+		const auto& dfs = [p, q](auto&& self, TreeNode* node) -> TreeNode* {
+			if(!node || node == p || node == q) return node; 
+			
+			TreeNode* left = self(self, node->left);
+			TreeNode* right = self(self, node->right);
+			
+			if(left && right) return node; 
+
+			return left ? left : right; 
+		};
+		
+		return dfs(dfs, root);	
 	}
 };
