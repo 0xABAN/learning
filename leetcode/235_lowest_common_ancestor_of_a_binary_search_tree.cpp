829f93a60fa2c46123764c83bc98839aff97dc4a
adam <admr0805@gmail.com>
2026-09-12 06:43:01 -0400

    feat(leetcode): solve bst lowest common ancestor
    follow bst ordering to locate the target split or ancestor
    without traversing unrelated subtrees

--- leetcode/235_lowest_common_ancestor_of_a_binary_search_tree.cpp
+++ leetcode/235_lowest_common_ancestor_of_a_binary_search_tree.cpp
@@ -9,7 +9,18 @@ struct TreeNode {
 class Solution {
 public:
 	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-		// TODO: implement
-		return nullptr;
+		const auto& dfs = [p, q](auto&& self, TreeNode* node) -> TreeNode* {
+			if (!node) return node; 
+			
+			if (node->val > p->val && node->val > q->val) {
+				return self(self, node->left);				
+			} else if (node->val < p->val && node->val < q->val) {
+				return self(self, node->right);	
+			} else {
+				return node; 	
+			}
+		};
+		
+		return dfs(dfs, root); 
 	}
 };
