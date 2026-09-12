f00c41b8bb9551a20585f0b7fa3797cf0eaf8661
adam <admr0805@gmail.com>
2026-09-12 04:43:02 -0400

    feat(leetcode): scaffold lowest common ancestor
    add the tree node type and method signature for revisiting
    problem 236 without including a solution

---
+++ leetcode/236_lowest_common_ancestor_of_a_binary_tree.cpp
@@ -0,0 +1,16 @@
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode(int val = 0, TreeNode* left = nullptr, TreeNode* right = nullptr)
+		: val(val), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		// TODO: implement
+		return nullptr;
+	}
+};
