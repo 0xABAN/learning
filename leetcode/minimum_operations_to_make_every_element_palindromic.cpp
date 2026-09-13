fb101f3c67f43e5137101daf84e89e27076a1597
adam <admr0805@gmail.com>
2026-09-12 23:10:49 -0400

    chore(leetcode): add palindromic operations stub
    provide the next contest scaffold without solution logic or tests

---
+++ leetcode/minimum_operations_to_make_every_element_palindromic.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        // TODO: implement
+        throw "not implemented";
+    }
+};
