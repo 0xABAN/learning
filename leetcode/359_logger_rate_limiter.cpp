ce784b25d2dcf7c0040724049e043773919dc6c5
adam <admr0805@gmail.com>
2026-09-13 09:36:51 -0400

    feat(leetcode): solve logger rate limiter
    track the last printed timestamp for each message to enforce the
    ten-second limit without extending it for rejected messages

--- leetcode/359_logger_rate_limiter.cpp
+++ leetcode/359_logger_rate_limiter.cpp
@@ -1,14 +1,18 @@
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Logger {
 public:
-	Logger() {
-
-	}
-
+	unordered_map<string, int> map; 
+	
 	bool shouldPrintMessage(int timestamp, string message) {
+		// we only rate limit the same message, not different messages 
+		// we will just use a hashmap	
 
+		if(map.contains(message) && timestamp - map[message] < 10) return false; 
+		map[message] = timestamp; 
+		return true; 
 	}
 };
