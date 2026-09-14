a7a2db7873e8107ffec9942b9720f77adb139705
adam <admr0805@gmail.com>
2026-09-14 15:18:42 -0400

    chore(leetcode): scaffold array zeroing problem
    add the checkArray stub for problem 2772 so it is ready for practice\nwithout including a solution

---
+++ leetcode/2772_apply_operations_to_make_all_array_elements_equal_to_zero.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool checkArray(vector<int>& nums, int k) {
+        // TODO: implement
+        throw "not implemented";
+    }
+};
