9bc2c75663dae19a792401ab0aaba7ee0a038cf4
adam <admr0805@gmail.com>
2026-09-14 23:22:26 -0400

    chore(leetcode): scaffold balanced binary tree
    add the tree node and isBalanced stub for problem 110
    without including a solution

---
+++ leetcode/110_balanced_binary_tree.cpp
@@ -0,0 +1,16 @@
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val = 0, TreeNode* left = nullptr, TreeNode* right = nullptr)
+        : val(val), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        // TODO: implement
+        throw "not implemented";
+    }
+};
