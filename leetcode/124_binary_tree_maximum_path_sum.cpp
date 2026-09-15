6028ec7c7dc8adb9ab188ca6af7a6a0c5cd92025
adam <admr0805@gmail.com>
2026-09-14 21:06:25 -0400

    chore(leetcode): scaffold maximum path sum
    add the tree node and maxPathSum stub for hard tree practice
    without including a solution

---
+++ leetcode/124_binary_tree_maximum_path_sum.cpp
@@ -0,0 +1,16 @@
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val = 0, TreeNode* left = nullptr, TreeNode* right = nullptr)
+        : val(val), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        // TODO: implement
+        throw "not implemented";
+    }
+};
