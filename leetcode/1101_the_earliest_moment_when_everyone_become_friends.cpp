9a858c40b7e2d7a7e12a2c8bfaf8a8f62b8daef1
adam <admr0805@gmail.com>
2026-09-15 04:57:55 -0400

    chore(leetcode): scaffold earliest acquaintance
    add the earliestAcq stub for problem 1101 so it is ready for practice
    without including a solution

---
+++ leetcode/1101_the_earliest_moment_when_everyone_become_friends.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int earliestAcq(vector<vector<int>>& logs, int n) {
+        // TODO: implement
+        throw "not implemented";
+    }
+};
